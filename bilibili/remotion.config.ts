751664a2fa7b8dc2feec826e793471165c6459ed
isomo <jiahaoxiang2000@gmail.com>
2026-01-30 19:35:36 +0800

    simple bilibili video remotion file

--- bilibili/remotion.config.ts
+++ bilibili/remotion.config.ts
@@ -4,8 +4,8 @@
 // Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 
 import { Config } from "@remotion/cli/config";
-import { webpackOverride } from "./src/remotion/webpack-override.mjs";
+import { enableTailwind } from '@remotion/tailwind-v4';
 
 Config.setVideoImageFormat("jpeg");
-
-Config.overrideWebpackConfig(webpackOverride);
+Config.setOverwriteOutput(true);
+Config.overrideWebpackConfig(enableTailwind);
