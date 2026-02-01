7aef6ee301dd2603663cbbb58120dc50631a8d7e
isomo <jiahaoxiang2000@gmail.com>
2026-02-01 18:16:17 +0800

    restart project

--- bilibili/src/plugins/index.ts
+++
@@ -1,3 +0,0 @@
-import type { PluginRegistration } from "../../types/plugin";
-
-export const pluginConfig: PluginRegistration[] = [];
--- bilibili/types/layout.ts
+++
@@ -1,8 +0,0 @@
-export type DockId = "left" | "center" | "right" | "bottom";
-
-export type PanelState = {
-  id: string;
-  title: string;
-  dock: DockId;
-  collapsed: boolean;
-};
